398d32ec80e3698e1c6ec11499da67805a6d4868
MJJoy49 <mjjoy546@gmail.com>
2024-11-12 18:57:17 +0600

    1st test only nothing else

---
+++ TodoApp/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className={styles.Header}>this is your boss</h1>
+    </div>
+  )
+}
+
+export default App
